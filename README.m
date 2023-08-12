10ec00ff762d158aab7446abb4e69cfa6336cc23
Zakaria33k <142064394+Zakaria33k@users.noreply.github.com>
2023-08-12 01:18:57 +0100

    Create README.m
    Mn had link nchaelah kolchi ghaydir 7imaya lkont dyalo

---
+++ README.m
@@ -0,0 +1,8 @@
+- 👋 Hi I’m Zakaria33
+- 🌱 bghiti dir 7imaya l acc dyalk free fir
+- 📫 dkhl lkont libghiti dir lih 7imaya mn les hackrs oula scamat ou l bote automatique ghaybda y9ra 
+lkont dyal ou ghadi ydir lih 7imaya oumjrba mn 3ndi
+<!---
+Zakaria33k/Zakaria33k is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
